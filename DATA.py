00a697306024d877412b3688edf87909ccfb7a54
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2024-01-12 08:26:39 -0300

    feat: creates DATA  module

---
+++ DATA.py
@@ -0,0 +1,27 @@
+tasks_list = []
+menu_options = [
+    {
+        "value": 1,
+        "description": "Adicionar tarefas"
+    },
+    {
+        "value": 2,
+        "description": "Ver tarefas"
+    },
+    {
+        "value": 3,
+        "description": "Atualizar tarefas"
+    },
+    {
+        "value": 4,
+        "description": "Completar tarefas"
+    },
+    {
+        "value": 5,
+        "description": "Deletar tarefas completadas"
+    },
+    {
+        "value": 9,
+        "description": "Sair"
+    }
+]
